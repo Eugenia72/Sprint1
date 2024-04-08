e3265dc80f6f77cf49b8b09bc751c8d921badb78
Eugenia72 <162119849+Eugenia72@users.noreply.github.com>
2024-04-08 14:30:20 +0200

    Add files via upload

---
+++ Tarea8/n1ej5/Functional_Interface.java
@@ -0,0 +1,6 @@
+package Tarea8.n1ej5;
+
+@FunctionalInterface
+interface Functional_Interface {
+    double getPiValue();
+}
--- Tarea8/n1ej5/Main.java
+++ Tarea8/n1ej5/Main.java
@@ -1,15 +1,12 @@
 package Tarea8.n1ej5;
-@FunctionalInterface
-interface PiValue {
-    double getPiValue();
-}
+
 
 public class Main {
     public static void main(String[] args) {
-        // Instanciar la interfaz y asignarle el valor 3.1415
-        PiValue pi = () -> 3.1415;
+        // Instanciar la interfaz
+        Functional_Interface pi = () -> 3.1415;
 
-        // Invocar el método getPiValue() e imprimir el resultado
+        // Invoco el método getPiValue() 
         System.out.println("El valor de Pi es: " + pi.getPiValue());
     }
 }
