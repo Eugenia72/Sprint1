ad31797ff647a9893a7098457977bc843ef09e44
Eugenia72 <162119849+Eugenia72@users.noreply.github.com>
2024-04-08 14:55:28 +0200

    Add files via upload

--- Tarea4/src/test/java/Tarea4/n1ej2/CalculoDniTest.java
+++ Tarea4/src/test/java/Tarea4/n1ej2/CalculoDniTest.java
@@ -1,22 +1,34 @@
 package Tarea4.n1ej2;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculoDniTest {
+
+    private CalculoDni calculoDni;
+
+    @BeforeEach//consulta Romina
+    public void setUp() {
+        calculoDni = new CalculoDni();
+    }
+
     @ParameterizedTest
     @CsvSource({
             "48096130, W",
             "46237920, P",
-            "87654321, B",
-            "98765432, C",
-            "23456789, D",
-            "54321678, E",
-            "65432187, F",
+            "49527158, Q",
+            "46005620, P",
+            "72055121, P",
+            "26453678, K",
+            "55931801, X",
             "45678923, A",
-            "41758262, Z",
-            "25879641, H"
+            "19533254, K",
+            "65117744, Y"
     })
     public void comprobarCalculoDni(int num, char letra) {
-        assertEquals(letra, CalculoDni.calcularLetraDni(num));
+        assertEquals(letra, calculoDni.calcularLetraDni(num));
     }
 }
